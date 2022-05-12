0919d45454d1acf569056a38ce1327da5fd5d8cb
hongyun <3478437011@qq.com>
2022-05-12 15:38:46 +0800

    vuepress 搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+// 配置网站的标题和描述，方便SEO
+
+module.exports = {
+    title:'操作手册',
+    description:'Typescript 操作手册',
+    themeConfig: {
+    //     nav: [
+    //         { text: '首页', link: '/' },
+    //         { 
+    //             text: '冴羽的 JavaScript 博客', 
+    //             items: [
+    //                 { text: 'Github', link: 'https://github.com/mqyqingfeng' },
+    //                 { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
+    //         ],
+    //     }
+    // ],
+        sidebar: [
+            {
+                title: '前言',
+                link: '/preface/introduction',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "系统介绍", path: "/preface/introduction" },
+                    { title: "应用场景", path: "/preface/scene" },
+                    { title: "使用前配置", path: "/preface/config" ,}
+                ]
+            },
+            {
+              title: "安装部署",
+              path: '/install/windows',
+              collapsable: false, // 不折叠
+              children: [
+                { title: "Windows下安装", path: "/install/windows" },
+                { title: "Linux下安装", path: "/install/linux" }
+              ],
+            }
+          ]
+    }
+    
+}
